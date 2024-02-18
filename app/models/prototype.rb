69eea2f918f4eab46e61d2e5739326087190ae2e
tsuyu6 <tsuyu.tsuyu.0606@gmail.com>
2024-02-18 21:39:59 +0900

    削除機能、完了

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,5 +2,5 @@ class Prototype < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   validates :image,:catch_copy,:concept,:title, presence: true
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
